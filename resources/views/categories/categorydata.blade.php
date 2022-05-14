4745122b6208f8d99781a3302bfd8d0100b19972
Onestring Lab <onestringlab@gmail.com>
2022-05-14 14:03:48 +0700

    All

---
+++ resources/views/categories/categorydata.blade.php
@@ -0,0 +1,30 @@
+@extends('../layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Categories Data</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Category</th>
+                <th scope="col">Created At</th>
+                <th scope="col">Updated At</th>
+                <th scope="col"><a href="#">Add</a></th>
+            </tr>
+        </thead>
+        <tbody>
+            @php ($no = 1)
+            @foreach ($categorydata as $category)
+            <tr>
+                <th scope="row">{{$no++}}</th>
+                <td>{{$category->category}}</td>
+                <td>{{$category->created_at}}</td>
+                <td>{{$category->updated_at}}</td>
+                <td><a href="#">Edit</a> | <a href="#">Del</a></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+
+use App\Http\Controllers\CategoriesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +24,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('categories', CategoriesController::class);
